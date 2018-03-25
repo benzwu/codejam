a44bd69f7f8e59748f4ed3740dba203b8bfab201
Jostein Topland <jostein.topland@gmail.com>
2018-03-25 12:13:22 +0200

    sum changes

--- js/primes/sketch.js
+++ js/primes/sketch.js
@@ -1,16 +1,16 @@
 var wave;
 var env;
 var time;
-var x, y, size;
+var x, y;
+var prime = 2;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	x = windowWidth / 2;
 	y = windowHeight / 2;
-	size = 10;
 
 	env = new p5.Env();
-	env.setADSR(0.001, 0.02, 0, 0);
+	env.setADSR(0.001, 0.05, 0, 0);
 	env.setRange(1, 0);
 
 	wave = new p5.Oscillator();
@@ -20,29 +20,55 @@ function setup() {
 	wave.amp(env);
 
 	noStroke();
-	colorMode(HSB, 1);
-	background(0);
+	colorMode(HSB);
+	background(240, 255, 255);
 }
 
 function draw() {
-	if (time != second()) {
-		time = second();
+    var size = 50;
+    var inc = 10;
+    var speed = 500;
+
+    var now = Math.floor(millis() / speed);
+	if (time != now) {
+		time = now;
 		env.play();
-		fill(color(time / 60, 255, 255));
-		switch (Math.floor(random(4))) {
+		switch (prime % 4) {
 			case 0:
 				x -= size;
+	            wave.freq(200);
 				break;
 			case 1:
 				x += size;
+	            wave.freq(300);
 				break;
 			case 2:
 				y -= size;
+	            wave.freq(400);
 				break;
 			case 3:
 				y += size;
+	            wave.freq(500);
 				break;
 		}
+	    colorMode(RGB);
+        var h = max(0, hue(get(x, y)) - inc);
+	    colorMode(HSB);
+		fill(h, 255, 255);
 		rect(x - size / 2, y - size / 2, size, size);
+        console.log(prime);
+        prime = nextPrime(prime);
 	}
+}
+
+function nextPrime(n) {
+    return 2;
+    do {
+        for (var i = 2; i < n; i++) {
+            if (n % i == 0) {
+                n++;
+                break;
+            }
+        }
+    } while (true);
 }
